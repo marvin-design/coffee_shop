62d34fe62b7ec13560e3eb5ffbed76e8c789aa83
marvin-design <marvindavid649@gmail.com>
2025-05-19 00:15:18 +0300

    commit

---
+++ customer.py
@@ -0,0 +1,37 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters.")
+
+    def orders(self):
+        return self._orders
+
+    def coffees(self):
+        return list({order.coffee for order in self._orders})
+
+    def create_order(self, coffee, price):
+        order = Order(self, coffee, price)
+        self._orders.append(order)
+        coffee._orders.append(order)
+        return order
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        customer_totals = {}
+        for order in coffee.orders():
+            customer = order.customer
+            customer_totals[customer] = customer_totals.get(customer, 0) + order.price
+        if not customer_totals:
+            return None
+        return max(customer_totals, key=customer_totals.get)
